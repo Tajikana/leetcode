ba24a1f615197b70f643f7c62cf50f1ef0d3c377
Pınar Yalçınduran <104770005+Tajikana@users.noreply.github.com>
2024-07-31 13:49:35 +0300

    Create 66.py

---
+++ 66.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if root==None:
+            return 0
+        count=1
+        
+        if root.left!=None:
+            count+=self.countNodes(root.left)
+        if root.right!=None:
+            count+=self.countNodes(root.right)
+        
+        return count
+        
